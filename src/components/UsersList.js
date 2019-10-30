03fa08bc2b7ae49d594015f3449adee2eaab1cb2
KacperDrewniak <50110237+KacperDrewniak@users.noreply.github.com>
2019-10-30 14:51:07 +0100

    Add Images

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,13 +1,15 @@
 import React from "react";
-
+import "./UsersList.css";
 const UsersList = props => {
   const users = props.users.map(user => (
     <div key={user.id.value}>
+      <img src={user.picture.large} alt="" srcset="" />
       <h4>{`${user.name.title} ${user.name.last}`}</h4>
+      <p>{user.email}</p>
     </div>
   ));
   console.log(props.users);
-  return <ul>{users}</ul>;
+  return <ul className="users">{users}</ul>;
 };
 
 export default UsersList;
